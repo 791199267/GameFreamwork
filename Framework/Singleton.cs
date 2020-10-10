af4466f19676944ca29be153ac0f8fca9bef12c6
李岩 <45325744+791199267@users.noreply.github.com>
2020-10-10 16:45:22 +0800

    Add files via upload

---
+++ Framework/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameFramwork
+{
+    public class Singleton<T> where T : new()
+    {
+        private static T _instance;
+
+        public static T GetInstance()
+        {
+            if (_instance == null)
+                _instance = new T();
+            return _instance;
+        }
+    }
+
+    public class SingletonMono<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+
+        public static T GetInstance()
+        {
+            if (_instance == null)
+            {
+                GameObject gameObj = new GameObject();
+                _instance = gameObj.AddComponent<T>();
+                gameObj.name = typeof(T).Name;
+                GameObject.DontDestroyOnLoad(gameObj);
+            }
+            return _instance;
+        }
+    }
+}
